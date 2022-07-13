a4f21f07ac09264f5ad4a62fbb9c7b532a9ad2d0
Anton <taco.v@yandex.ru>
2022-07-13 13:41:32 +0300

    feat: add descriptions for tasks lesson 8

--- Lesson8/js/taskTimer.js
+++ Lesson8/js/taskTimer.js
@@ -1,3 +1,4 @@
+// Написать функцию, которая принимает год, а возращает день 1го января этого года
 const week = ['воскресенье',
     'понедельник',
     'вторник',
@@ -13,6 +14,8 @@ function getWeekDayByYear(year) {
 
 // console.log(getDate(2016));
 
+// Написать функцию, которая принимает дату в виде строки "29.03.1985",
+// а возвращает день в русском формате.
 function getWeekDayByDate(date) {
     let pattern = /(\d{2})\.(\d{2})\.(\d{4})/;
     let dt = new Date(date.replace(pattern, '$3-$2-$1'));
@@ -22,6 +25,8 @@ function getWeekDayByDate(date) {
 
 // console.log(getWeekDayByDate('29.03.1985'))
 
+
+// Узнать, сколько прошло полных недель с начала года до сегодняшней даты
 function getWeeksCount(year) {
     let oldTime = new Date(year, 0);
     let newTime = new Date();
@@ -30,6 +35,9 @@ function getWeeksCount(year) {
 
 // console.log(getWeeksCount(2016));
 
+
+// Напишите код, который будет выводить в консоль строку, начиная с
+// одной буквы и добавлять по одному символу каждые 200 мc
 function getTextWithDelay(text) {
     for (let i = 0; i < text.length; i++) {
         setTimeout(() => {
@@ -40,12 +48,19 @@ function getTextWithDelay(text) {
 
 // getTextWithDelay('text');
 
+
+// Создайте функцию, которая принимает строку и время в секундах.
+// При вызове функции она должна показать переданный текст через заданное
+// количество секунд
 function myFunc(string, time) {
     return setTimeout(() => console.log(string), time);
 }
 
 // myFunc('test', 1000);
 
+
+//Создать наследника от Element - новый класс TextElement.
+//Этот класс работает со строками. Добавьте ему свойство length, которое будет равно this.content.length. Пусть метод setContent выполняет все то же, что и у Element + устанавливает новый this.length = value.length.
 function Element(content) {
     this.content = content;
     this.elementType = 'html';
