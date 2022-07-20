e3f6725c4ea59c398d813f381df881fc96347c3f
Anton <taco.v@yandex.ru>
2022-07-20 16:20:48 +0300

    fix: fix structure of Portfolio for mobile

--- Portfolio/js/main.js
+++ Portfolio/js/main.js
@@ -2,7 +2,8 @@ let mainMenu = document.querySelector('.header__list');
 let mainHamburger = document.querySelector('.hamburger');
 
 mainHamburger.addEventListener('click', () => {
-    mainMenu.classList.toggle('header__list--active')
+    mainMenu.classList.toggle('header__list--active');
+    mainHamburger.classList.toggle('hamburger--active');
 })
 
 let modal = document.querySelector('.modal')
@@ -11,17 +12,31 @@ let close = document.querySelector('.btn-close');
 btn.addEventListener('click', () => {
     modal.style.display = 'block';
 })
+
 close.addEventListener('click', () => {
+    document.querySelector('.feedback__form').reset();
     modal.style.display = 'none';
 });
 
+modal.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+        document.querySelector('.feedback__form').reset();
+        modal.style.display = 'none';
+    }
+})
+
 let menuLinks = document.querySelectorAll('.header__link');
 
-for (let menuLink in menuLinks) {
+for (const menuLink of menuLinks) {
     menuLink.addEventListener('click', (e) => {
         e.preventDefault();
-        let scrollToElement = menuLink.getAttribute('href');
-        let coords = document.querySelector(scrollToElement).offsetTop;
+        const scrollToElement = menuLink.getAttribute('href');
+        const coords = document.querySelector(scrollToElement).offsetTop;
         window.scrollTo({top: coords - 100, behavior: "smooth"})
     })
 }
+let name = document.querySelector('.name__value').value;
+let phone = document.querySelector('.phone__value').value;
+let submit = document.querySelector('.btn-submit');
+
+
